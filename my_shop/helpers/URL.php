a1845ea666530a1ba4ea343828c28f8a32482143
yoo16 <yohei.yoshikawa@gmail.com>
2021-09-28 16:02:46 +0900

    UC

--- my_shop/helpers/URL.php
+++ my_shop/helpers/URL.php
@@ -8,29 +8,4 @@ class URL
         return $url;
     }
 
-    //ラジオボタンの選択
-    public static function checked($value, $target)
-    {
-        if ($value == $target) {
-            return 'checked';
-        }
-    }
-
-    //プルダウンの選択
-    public static function selected($value, $target)
-    {
-        if ($value == $target) {
-            return 'selected';
-        }
-    }
-
-    //性別のラベル表示
-    public static function gender($key)
-    {
-        $genders = ['male' => '男性', 'female' => '女性'];
-        if (isset($genders[$key])) {
-            return $genders[$key];
-        }
-        return '';
-    }
 }
